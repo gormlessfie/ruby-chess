996572d297f1bb2e6f48383042fad8fa4ac930cc
gormlessfie <gmlessfie@gmail.com>
2021-11-15 20:39:43 -0800

    Create game class

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require './lib/board'
+require './lib/player'
+require './lib/game_logic'
+
+# This holds all the methods that runs the game such as turns.
+class Game
+end
