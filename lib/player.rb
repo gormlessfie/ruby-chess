697ad7cdbe02e7721856eabe81d09550bc0c58f3
gormlessfie <gmlessfie@gmail.com>
2021-11-15 20:37:34 -0800

    removed merge stuff

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,7 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
-=======
-# A player that is either white or black. White goes first.
->>>>>>> player
 class Player
   def initialize(color)
     @color = color
   end
-<<<<<<< HEAD
-
-  
-=======
->>>>>>> player
 end
