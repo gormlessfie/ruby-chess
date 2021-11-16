0027001f093461d19626c58cc8b6040c778e328d
gormlessfie <gmlessfie@gmail.com>
2021-11-15 16:03:30 -0800

    Switch pawn and royal row

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require './lib/chess_pieces/pawn'
+require './lib/chess_pieces/bishop'
+require './lib/chess_pieces/king'
+require './lib/chess_pieces/queen'
+require './lib/chess_pieces/knight'
+require './lib/chess_pieces/rook'
+
+
 class Board
   def initialize
     @board = initialize_board
@@ -8,15 +16,17 @@ class Board
   def initialize_board
     board = Array.new(8) { Array.new(8) }
 
-    board[0] = [Rook.new, Knight.new, Bishop.new, Queen.new,
-                King.new, Bishop.new, Knight.new, Rook.new]
-    board[1] = [Pawn.new, Pawn.new, Pawn.new, Pawn.new,
-                Pawn.new, Pawn.new, Pawn.new, Pawn.new]
+    board[0] = [Rook.new('black'), Knight.new('black'), Bishop.new('black'),
+                Queen.new('black'), King.new('black'), Bishop.new('black'),
+                Knight.new('black'), Rook.new('black')]
+    board[1] = Array.new(8) { Pawn.new('black') }
 
-    board[0] = [Rook.new, Knight.new, Bishop.new, Queen.new,
-                King.new, Bishop.new, Knight.new, Rook.new]
-    board[7] = [Rook.new, Knight.new, Bishop.new, Queen.new,
-                King.new, Bishop.new, Knight.new, Rook.new]
+    board[6] = Array.new(7) { Pawn.new('white') }
+    board[7] = [Rook.new('white'), Knight.new('white'), Bishop.new('white'),
+                Queen.new('white'), King.new('white'), Bishop.new('white'),
+                Knight.new('white'), Rook.new('white')]
+
+    board
   end
 end
 
@@ -24,5 +34,5 @@ b = Board.new
 
 te = b.instance_variable_get(:@board)
 te.each do |row|
-  p row
+  puts row
 end
