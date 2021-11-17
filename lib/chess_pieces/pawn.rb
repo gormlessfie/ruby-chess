9656d85efeddecd5e749209439d59d9931c6a0a6
gormlessfie <gmlessfie@gmail.com>
2021-11-16 23:06:46 -0800

    Pawn possible moves

--- lib/chess_pieces/pawn.rb
+++ lib/chess_pieces/pawn.rb
@@ -5,13 +5,36 @@ class Pawn
     @name = 'pawn'
     @color = color
     @current_pos = index
-    @possible_moves = update_possible_moves
+    @possible_moves = create_possible_moves(@current_pos)
     @icon = determine_icon
     @first_turn = true
   end
 
   def update_possible_moves
 
+  end
+
+  def create_possible_moves(index)
+    white_key = [[-1, 0], [-2, 0]]
+    black_key = [[1, 0], [2, 0]]
+
+    if @color.match('white')
+      white_key.map do |possible_move|
+        pos_row = possible_move[0] + index[0]
+        pos_col = possible_move[1] + index[1]
+        next unless pos_row.between?(0, 7) && pos_col.between?(0, 7)
+
+        [pos_row, pos_col]
+      end
+    else
+      black_key.map do |possible_move|
+        pos_row = possible_move[0] + index[0]
+        pos_col = possible_move[1] + index[1]
+        next unless pos_row.between?(0, 7) && pos_col.between?(0, 7)
+        
+        [pos_row, pos_col]
+      end
+    end
   end
 
   def update_first_turn_false
@@ -29,3 +52,7 @@ class Pawn
     @icon
   end
 end
+
+paw = Pawn.new('black', [6, 3])
+
+p paw.create_possible_moves([6, 3])
