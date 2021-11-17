75195e059fb9ed9be48a80d3cb4c9092509ed27f
gormlessfie <gmlessfie@gmail.com>
2021-11-16 23:17:49 -0800

    Made first turn have unique move pawn

--- lib/chess_pieces/pawn.rb
+++ lib/chess_pieces/pawn.rb
@@ -5,7 +5,9 @@ class Pawn
     @name = 'pawn'
     @color = color
     @current_pos = index
-    @possible_moves = create_possible_moves(@current_pos)
+    @white_key = [[-1, 0], [-2, 0]]
+    @black_key = [[1, 0], [2, 0]]
+    @possible_moves = create_possible_moves(@current_pos, @white_key, @black_key)
     @icon = determine_icon
     @first_turn = true
   end
@@ -14,31 +16,36 @@ class Pawn
 
   end
 
-  def create_possible_moves(index)
-    white_key = [[-1, 0], [-2, 0]]
-    black_key = [[1, 0], [2, 0]]
-
+  def create_possible_moves(current_position, white_key, black_key)
     if @color.match('white')
-      white_key.map do |possible_move|
-        pos_row = possible_move[0] + index[0]
-        pos_col = possible_move[1] + index[1]
-        next unless pos_row.between?(0, 7) && pos_col.between?(0, 7)
-
-        [pos_row, pos_col]
-      end
+      possible_moves_helper(white_key, current_position)
     else
-      black_key.map do |possible_move|
-        pos_row = possible_move[0] + index[0]
-        pos_col = possible_move[1] + index[1]
-        next unless pos_row.between?(0, 7) && pos_col.between?(0, 7)
-        
-        [pos_row, pos_col]
-      end
+      possible_moves_helper(black_key, current_position)
+    end
+  end
+
+  def possible_moves_helper(key, current_position)
+    key.map do |possible_move|
+      pos_row = possible_move[0] + current_position[0]
+      pos_col = possible_move[1] + current_position[1]
+      next unless pos_row.between?(0, 7) && pos_col.between?(0, 7)
+
+      [pos_row, pos_col]
     end
   end
 
   def update_first_turn_false
     @first_turn = false
+    update_white_key
+    update_black_key
+  end
+
+  def update_white_key
+    @white_key.pop
+  end
+
+  def update_black_key
+    @black_key.pop
   end
 
   def determine_icon
@@ -53,6 +60,6 @@ class Pawn
   end
 end
 
-paw = Pawn.new('black', [6, 3])
+paw = Pawn.new('black', [1, 3])
 
-p paw.create_possible_moves([6, 3])
+p paw.instance_variable_get(:@possible_moves)
