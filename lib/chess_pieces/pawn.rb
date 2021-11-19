3b40430d9961f4a9f9f9f66819e7ce4005fb2c19
gormlessfie <gmlessfie@gmail.com>
2021-11-19 15:38:00 -0800

    Add read access to pawn first turn

--- lib/chess_pieces/pawn.rb
+++ lib/chess_pieces/pawn.rb
@@ -6,6 +6,7 @@ require './lib/chess_pieces/chess_pieces'
 # and 1 space afterwards. This piece can attack diagonally, if there is a
 # black piece 1 space diagonal from it in front.
 class Pawn < ChessPieces
+  attr_reader :first_turn
   def initialize(color, index)
     white_key = create_white_key
     black_key = create_black_key
