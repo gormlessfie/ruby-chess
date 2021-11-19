97f0f0d49d8464a2b4bada6f8d179dd05190c027
gormlessfie <gmlessfie@gmail.com>
2021-11-19 14:16:12 -0800

    Remove redundant read access

--- lib/chess_pieces/pawn.rb
+++ lib/chess_pieces/pawn.rb
@@ -6,8 +6,6 @@ require './lib/chess_pieces/chess_pieces'
 # and 1 space afterwards. This piece can attack diagonally, if there is a
 # black piece 1 space diagonal from it in front.
 class Pawn < ChessPieces
-  attr_reader :name, :color, :current_pos, :possible_moves, :first_turn
-
   def initialize(color, index)
     white_key = create_white_key
     black_key = create_black_key
