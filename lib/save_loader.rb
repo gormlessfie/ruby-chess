bfb7e85b0b6d88e1389618b3bd28176bb0b2c3f9
gormlessfie <gmlessfie@gmail.com>
2021-11-24 22:20:25 -0800

    Add basic save/load functionality

---
+++ lib/save_loader.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'date'
+
+# This saves and loads the game. This will save the current state of the board.
+class SaveLoader
+  def initialize(game)
+    @game = game
+  end
+
+  def setup_save_folder
+    create_save_dir
+  end
+
+  def save_game
+    serialized_object = Marshal.dump(@game)
+    Dir.chdir('saves')
+
+    File.open(user_save_name_choice, 'w') { |file| file.write(serialized_object) }
+
+    print '       '
+    puts "Save complete! You can exit at any time."
+  end
+
+  def load_game
+    save_list = Dir.children('saves')
+    save_list.each_with_index { |save_name, idx| puts "       #{idx + 1} : #{save_name}" }
+
+    user_input = save_user_choice(save_list)
+
+    save = File.open(save_list[user_input - 1].to_s)
+    Marshal.load(save)
+  end
+
+  private
+
+  def create_save_dir
+    Dir.mkdir('saves') unless Dir.exist?('saves')
+  end
+
+  def save_user_choice(save_list)
+    print '       '
+    print 'Please choose a save to load: '
+    loop do
+      input = gets.chomp.to_i
+      return input if input.between?(1, save_list.length)
+
+      print '       '
+      puts 'You must select a choice that is on the list.'
+    end
+  end
+
+  def user_save_name_choice
+    print '       '
+    print 'Please enter a name for your save: '
+
+    gets.chomp
+  end
+end
