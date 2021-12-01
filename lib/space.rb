0ef4900c45fe34ad880881ff30ddb27f235a6f02
gormlessfie <gmlessfie@gmail.com>
2021-11-30 16:01:16 -0800

    Add read access to space color

--- lib/space.rb
+++ lib/space.rb
@@ -3,6 +3,7 @@
 # A square in the board. The space object has the color of the space and
 # the piece it is holding, if any.
 class Space
+  attr_reader :color
   attr_accessor :piece
 
   def initialize(color = 'white', piece = nil)
